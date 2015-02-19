1af4f5b8b622ffd700e7c6cd2a0304d3154b3329
Cuan-Chai Megghross <iamcuan@gmail.com>
2015-02-19 06:48:25 -0500

    Replacing Emails with Names

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,4 +3,7 @@ class Pin < ActiveRecord::Base
 
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/system/pins/images/000/000/005/medium/missing.jpg"
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+
+	validates :image, presence: true
+  validates :description, presence: true
 end
